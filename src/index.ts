dea8b19fad28dbc79eef904d335e24b535741606
Bart6114 <bart@dataroots.io>
2023-07-10 20:38:27 +0200

    add rest of params to action yaml

--- src/index.ts
+++ src/index.ts
@@ -9,11 +9,14 @@ try {
   const ghTokenCommits: string = core.getInput('gh_token_commits')
   const badgePath: string = core.getInput('badge_path')
   const commitMessage: string = core.getInput('commit_message')
+  const header: string = core.getInput('header')
+  const about: string = core.getInput('about')
 
   const stats = await collectStats({ ghToken: ghTokenStats, username })
 
   const svgContent = await generateSVG({
-    about: 'He/him, cheese, dad, data,\nrocks & trails.',
+    header,
+    about,
     stats,
     username
   })
